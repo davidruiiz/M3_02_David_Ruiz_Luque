2d95c9695dd98d577d547a21c02c6ab61b1d32a2
David Ruiz <113980722+davidruiiz@users.noreply.github.com>
2022-11-20 23:13:35 +0100

    cambios finales

--- ejercicio3.py
+++ ejercicio3.py
@@ -6,7 +6,7 @@ print('\nEJERCICIO 3\n')
 paises = { "españa":"español", "eeuu":"inglés", "italia":"italiano" }
 
 def imprimir_clave_valor(paises, valor):
-    ##Utilizamos try y except para controlar las excepciones
+    #Utilizamos try y except para controlar las excepciones
     try:
         return paises[valor] #Se accede al valor de una clave dada
 
--- ejercicio4.py
+++ ejercicio4.py
@@ -4,8 +4,18 @@ import sys
 print('\nEJERCICIO 4\n')
 
 def suma(a,b):
-    try:
+    try: #Utilizamos try y except para controlar las excepciones
         return a+b
-    except TypeError: 
+    except TypeError: #En el caso de que uno de los dos números sea de tipo cadena o booleano saltará la excepción TypeError 
+        print('No se puede concatenar un "string" con un "int".') #En tal caso el programa imprimirá este mensaje
         return '\nOperación no válida.'
-        
+        sys.exit()
+
+#Ejemplo:
+print(suma('3',5))
+
+#Importamos a main
+
+if __name__=='__main__':
+    main()
+
