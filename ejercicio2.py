a2642438befd58567045c90074389bd94e4ef668
David Ruiz <113980722+davidruiiz@users.noreply.github.com>
2022-11-20 21:00:02 +0100

    cambios

---
+++ ejercicio2.py
@@ -0,0 +1,22 @@
+from ast import main
+import main
+
+print('EJERCICIO 2\n')
+
+lista=[4, 7, 30, 23, 5]
+
+def imprimir_elemento_lista(lista, posicion):
+    ##Utilizamos try y except para controlar las excepciones
+    try:
+        return lista[posicion] 
+    except IndexError: #En caso de que el índice dado exceda la longitud de la lista, usamos IndexError para controlar la excepción
+        print('El índice está fuera del rango.') #cuando esto ocurra se lanzará este mensaje
+
+#Ejemplo
+print(imprimir_elemento_lista(lista, 11))
+
+#Importamos a main
+if __name__=="__main__":
+    main()
+
+
