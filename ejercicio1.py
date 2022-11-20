1c4d79276e4a2cfe9f07565c6a4ae7a249c8092d
David Ruiz <113980722+davidruiiz@users.noreply.github.com>
2022-11-20 20:36:02 +0100

    acabar ejercicio 1

--- ejercicio1.py
+++ ejercicio1.py
@@ -10,3 +10,11 @@ def dividir(a,b):
 
     except ZeroDivisionError: #En el caso de que b=0 se ejecutará la excepción ya que sería un error del tipo ZeroDivisonError
         print('No es posoble dividir entre cero.') #cuando el denominador sea 0, el programa lanzará este mensaje.
+
+#Ejemplo
+
+print(dividir(7, 0))
+
+#Importamos a main
+if __name__ == '__main__':
+    main()
