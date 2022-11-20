7d3f2f9d3226227bcd96b334e11bbda1152b3a5c
David Ruiz <113980722+davidruiiz@users.noreply.github.com>
2022-11-20 21:22:47 +0100

    Update ejercicio3.py

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,2 +1,19 @@
 from ast import main
 import sys
+
+print('\nEJERCICIO 3\n')
+
+paises = { "españa":"español", "eeuu":"inglés", "italia":"italiano" }
+
+def imprimir_clave_valor(lista, pais):
+    ##Utilizamos try y except para controlar las excepciones
+    try:
+        return lista[pais] #Se accede al valor de una clave dada
+
+    except KeyError: #En el caso de no encontrar el valor, usamos KeyError para controlar la excepción
+        return 'Valor no encontrado.' #En ese caso, el programa lanzará este mensaje
+        sys.exit()
+
+
+
+
